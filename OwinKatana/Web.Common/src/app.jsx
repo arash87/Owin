4dacb852f8aba75bfe69a2b977a545d93528a1e7
arash_farmani <arash_farmani@msn.com>
2018-07-18 20:00:20 +0200

    React and react router in place in web.common

---
+++ OwinKatana/Web.Common/src/app.jsx
@@ -0,0 +1,18 @@
+﻿import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import BasicExample from './BasicRoutingExample.jsx'
+
+class App extends Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return (
+			<h2>React is in its place!!!</h2>
+			)
+	}
+}
+
+ReactDOM.render(<BasicExample />, document.getElementById('root'));
+
---
+++ OwinKatana/Web.Common/webpack.config.js
@@ -0,0 +1,26 @@
+﻿var path = require('path')
+
+module.exports = {
+	entry: ['./src/app.jsx'],
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, '../Web/dist')
+	},
+	devtool: 'source-maps',
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react']
+					}
+				}
+			}
+		]
+	}
+
+}
