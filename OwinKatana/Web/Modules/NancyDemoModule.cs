ed7b1ca962236dc69186d66c1f0d83541db70f80
arash_farmani <arash_farmani@msn.com>
2018-07-18 08:37:18 +0200

    added nancyfx

---
+++ OwinKatana/Web/Modules/NancyDemoModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace Web.Modules
+{
+	public class NancyDemoModule : NancyModule
+	{
+		public NancyDemoModule()
+		{
+			Get["/"] = _ =>
+			{
+				return View["index"];
+			};
+		}
+	}
+}
---
+++ OwinKatana/Web/scripts/script.js
@@ -0,0 +1,3 @@
+﻿(function() {
+	console.info("hello world");
+})();
